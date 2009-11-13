c0ff94d12293f88e4e708662915a0f5a49320af3
Christopher Vollick <psycotica0@macbook.workgroup>
2009-11-13 11:50:06 -0500

    Pick Can Now Pick More Than 1 Item.
    Running Pick over the same input multiple times to pick multiple values was dumb.
    There were problems with randomness, and it was inefficient.
    
    Now, the recommended way is to run 'pick n' instead.
    
    Probably one of the more confusing changes in this patch is the difference in what the variable "current" does.
    Previously "current" held the current value that was to be output, and "temp" held the input lines.
    
    Now, though, "current" holds the current input line, and "values" is an array of the registers to output.

--- pick.c
+++ pick.c
@@ -9,9 +9,36 @@ int randBetween (int low, int high) {
 }
 
 int main (int argc, char* argv[]) {
+	/* This holds the current line */
 	string_chunk* current = new_chunk_string();
-	string_chunk* temp = new_chunk_string();
+	/* This is my array of output registers */
+	string_chunk** values = NULL;
+	/* This is the number of random values we've been asked for */
+	long requested = 1;
+	/* This is the number of lines we've read thus far. */
 	int number = 0;
+	/* This is just a counter for the for loops. */
+	long i;
+
+	if (argc > 1) {
+		char* end;
+		requested = strtol(argv[1], &end, 10);
+		if (end == argv[1]) {
+			/* Invalid Number */
+			fputs("Only Argument Must Be A Number\n", stderr);
+			exit(1);
+		} else if (requested <= 0) {
+			fputs("Number Must Be A Positive Integer\n", stderr);
+			exit(1);
+		}
+	}
+
+	/* Allocate All My Value Registers */
+	values = malloc(sizeof(string_chunk*) * requested);
+	/* And Initialize them to empty strings */
+	for(i = 0; i < requested; i++) {
+		values[i] = new_chunk_string();
+	}
 
 	#ifdef S_RAND_DEV
 	/* I like srandomdev better, but it's only on BSD */
@@ -30,18 +57,23 @@ int main (int argc, char* argv[]) {
 	#endif
 
 	while (!feof(stdin)) {
-		if (chunk_readline(stdin, temp) > 0) {
+		if (chunk_readline(stdin, current) > 0) {
 			/* Not a blank line */
 			number++;
-			/* Now we make a random number between 1 and number. If it's 1, we swap temp and current. Otherwise, we leave it as it is. */
-			/* This should, as number increases, guarantee that by the end, all lines will have had equal 1/n chance of making it to the end. */
-			if (randBetween(1,number) == 1) {
-				/* We should now copy temp to current */
-				chunk_string_copy(temp, current);
+			for (i = 0; i < requested; i++) {
+				/* Now we make a random number between 1 and number. If it's 1, we copy the current line to the current register. Otherwise, we leave it as it is. */
+				/* This should, as number increases, guarantee that by the end, all lines will have had equal 1/n chance of making it to the end. */
+				if (randBetween(1,number) == 1) {
+					/* We should now copy current to the value register */
+					chunk_string_copy(current, values[i]);
+				}
 			}
 		}
 	}
 
-	chunk_printline(stdout, current);
+	for (i = 0; i < requested; i++) {
+		chunk_printline(stdout, values[i]);
+		putc('\n', stdout);
+	}
 	return 0;
 }
